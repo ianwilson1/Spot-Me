f48656b69d8c6a0239fc7a61f63129189be4cef1
Adrian D <adalena2002@gmail.com>
2024-11-03 17:40:17 -0800

    Prepared server program for database interaction.

--- Server/gateway-sim.py
+++ Server/gateway-sim.py
@@ -1,8 +1,7 @@
 # Summary of operation:
 #   - Pick a random spot from the list of spots by ID.
-#   - Initialize a "ParkingSpot" dictionary object initialized with this ID.
-#   - Pick a random state to change it to (0 = free, 1 = occupied, 2 = reserved)
-#   - Check if this spot is in the list of handicap spots. Set the isHandicap attribute accordingly.
+#   - Initialize a "ParkingSpot" dictionary object initialized with this ID
+#     and a random state to change it to (0 = free, 1 = occupied, 2 = reserved)
 #   - Stringify this dictionary into a JSON string.
 #   - Send this JSON string to the server.
 #   - Repeat every 3 seconds.
@@ -13,15 +12,15 @@ import socket
 import time
 
 #################################################### Constants
-HEADER = 128
+HEADER = 64
 PORT = 15024
 FORMAT = 'utf-8'
-SERVER = socket.gethostbyname(socket.gethostname()) # <- put public address of server machine here
+SERVER = '34.105.119.88' # <- put public address of server machine here
+SERVER = socket.gethostbyname(socket.gethostname()) # For local machine testing only; COMMENT OUT WHEN TESTING ON SERVER
 ADDR = (SERVER, PORT)
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
 MAX_SPOTS = 10          # Number of parking spots we currently have implemented. CHANGE AS NEEDED
-HANDICAP_SPOTS = []     # List of handicap spots, if any are implemented. CHANGE AS NEEDED
 ####################################################
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,9 +40,9 @@ while True:
     spotID = random.randint(1, MAX_SPOTS)
 
     updSpotInfo = {
+        "op": "UpdateSpot",
         "id": spotID,
         "status": random.randint(0,2),
-        "isHandicap": spotID in HANDICAP_SPOTS
     }
     
     msg = json.dumps(updSpotInfo)
--- Server/server.py
+++ Server/server.py
@@ -1,48 +1,75 @@
+# List of operations:
+#   - UpdateSpot: update a spot's occupancy status
+#   - CreateAccount: create a new account
+#   - UpdateName: update a user's name
+#   - UpdatePass: update a user's password
+#   - UpdatePermit: update a user's list of permits
+
 import socket
 import threading
 import json
+import pymongo
 
 #################################################### Constants
-HEADER = 128
+HEADER = 64
 PORT = 15024
 SERVER = socket.gethostbyname(socket.gethostname()) # Get the local IPv4 of current machine
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
+
+#DB_CLIENT = pymongo.MongoClient('localhost', 27017)
+#DATABASE = DB_CLIENT['']
+#COLLECTION = DATABASE['']
 ####################################################
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected")
+def UpdateSpot(id, status):
+    print(f"[OPERATION] UpdateSpot({id},{status})")
 
+    # TODO: Implement database interaction
+
+def HandleOperation(rcvdJson):
+    if rcvdJson["op"] == "UpdateSpot":
+        id = rcvdJson["id"]
+        status = rcvdJson["status"]
+        UpdateSpot(id, status)
+
+    # TODO: Create branches for the rest of the operations
+
+def HandleClient(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected")
     connected = True
+
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
 
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
+        if not msg_length: # No message contents received
+            continue
 
-            print(f"[{addr}] {msg}")
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+
+        if msg == DISCONNECT_MESSAGE:
+            connected = False
+
+        rcvdJson = json.loads(msg)
+        HandleOperation(rcvdJson)
 
     conn.close()
-    
 
-def start():
-    server.listen() # Listen for new connections
+def Start():
+    server.listen()
     print(f"[LISTENING] Listening on {SERVER}")
     while True:
-        # Store connection and socket to communicate back
         conn, addr = server.accept() 
 
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread = threading.Thread(target=HandleClient, args=(conn, addr))
         thread.start()
 
         print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
 print("[STARTING]")
-start()
+Start()
